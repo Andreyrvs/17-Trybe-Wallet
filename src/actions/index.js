e2ac21072209980bcf0d031bcc3db1ee0f28a7b7
Andreyrvs <cortaesai@gmail.com>
2022-03-01 04:56:11 -0300

    Requisito 01 - Feature (Redux): Adiciona estrutura das action com thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,23 @@
-// Coloque aqui suas actions
+function /* nome da action1 */(data) {
+  return { type: /* tipo da action */, payload: data }
+}
+
+function /* nome da action2 */() {
+  return async (dispatch) => {
+
+    /* ===> despacha action com o erro */
+
+    try {
+
+      /* ===> requisição da API */
+      /* ===> despacha action com os dados do personagem */
+
+    } catch (error) {
+
+      /* ===> despacha action com o erro */
+
+    }
+  }
+}
+
+export default /* nome da action2 */;
