c4a1ff45437d415b0f9a589a9f852df75ed1a65f
Andrey Visniewski <cortaesai@gmail.com>
2023-02-27 00:43:48 -0300

    fix: tela de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import logo from './logoWallet.png';
 import Input from '../components/Input';
 import Button from '../components/Button';
 import fetchAPI from '../services';
 import { userEmail } from '../actions';
+import './login.css';
 
 const PASSWORD_LIMIT = 6;
 class Login extends React.Component {
@@ -58,38 +60,53 @@ class Login extends React.Component {
   render() {
     const { email, isDisable, password } = this.state;
     return (
-      <form onSubmit={ (event) => this.handleSubmit(event) }>
+      <main className="home">
+        <section className="page">
 
-        <Input
-          dataTest="email-input"
-          elementId="input-email"
-          inputType="email"
-          labelName="Email"
-          name="email"
-          onInputChange={ this.handleChange }
-          inputValue={ email }
-        />
+          <form
+            onSubmit={ (event) => this.handleSubmit(event) }
+            className="form"
+          >
+            <img src={ logo } alt="logo" />
+            <section>
 
-        <Input
-          autoComplete="off"
-          dataTest="password-input"
-          elementId="input-password"
-          inputType="password"
-          labelName="Senha"
-          name="password"
-          onInputChange={ this.handleChange }
-          inputValue={ password }
-        />
+              <Input
+                dataTest="email-input"
+                elementId="input-email"
+                inputType="email"
+                labelName="Email"
+                name="email"
+                onInputChange={ this.handleChange }
+                inputValue={ email }
+              />
+            </section>
+            <section>
 
-        <Button
-          bsClass="btn btn-primary"
-          elementId="btn-login"
-          isDisable={ isDisable }
-          btnType="submit"
-        >
-          Entrar
-        </Button>
-      </form>
+              <Input
+                autoComplete="off"
+                dataTest="password-input"
+                elementId="input-password"
+                inputType="password"
+                labelName="Senha"
+                name="password"
+                onInputChange={ this.handleChange }
+                inputValue={ password }
+              />
+            </section>
+
+            <section>
+              <Button
+                bsClass="btn btn-primary"
+                elementId="btn-login"
+                isDisable={ isDisable }
+                btnType="submit"
+              >
+                Entrar
+              </Button>
+            </section>
+          </form>
+        </section>
+      </main>
     );
   }
 }
